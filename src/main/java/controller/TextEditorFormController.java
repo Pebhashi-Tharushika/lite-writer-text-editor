4096745e5fb6439c93fc51d091288ce26cba59a8
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2024-09-27 15:23:18 +0530

    add find and replace functionalties

--- src/main/java/controller/TextEditorFormController.java
+++ src/main/java/controller/TextEditorFormController.java
@@ -1,31 +1,35 @@
 package controller;
 
 import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.print.PageLayout;
 import javafx.print.Printer;
-import javafx.print.PrinterAttributes;
 import javafx.print.PrinterJob;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.input.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.HTMLEditor;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import util.SearchResult;
 
-import java.awt.print.PageFormat;
 import java.io.*;
 import java.net.URL;
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TextEditorFormController {
     public MenuItem mnuNew;
@@ -38,14 +42,51 @@ public class TextEditorFormController {
     public MenuItem mnuCopy;
     public MenuItem mnuPaste;
     public MenuItem mnuSelectAll;
+    public MenuItem mnuFindAndReplace;
     public MenuItem mnuAbout;
     public HTMLEditor txtEditor;
 
     private final Clipboard clipboard = Clipboard.getSystemClipboard();
+
+    public Button btnDown;
+    public Button btnUp;
+    public CheckBox chkMatchCase;
+    public TextField txtFind;
+    public TextField txtReplace;
+    public Button btnReplace;
+    public Button btnReplaceAll;
+    public Label lblResults;
+    public TabPane pneFindAndReplace;
+    public AnchorPane pneContainer;
+    public Tab pneTab;
     private Path currentFilePath;
     private WebEngine webEngine;
 
+    private final ArrayList<SearchResult> searchResultList = new ArrayList<>();
+    private int pos = 0;
+
     public void initialize() {
+
+        removeTabPane();
+
+        txtFind.textProperty().addListener((ov, previous, current) -> calculateSearchResult());
+
+        // Create a StringProperty to hold the HTML content
+        StringProperty htmlContentProperty = new SimpleStringProperty();
+
+        // Bind the StringProperty to the HTMLEditor's HTML content
+        htmlContentProperty.bind(Bindings.createStringBinding(
+                txtEditor::getHtmlText,  // Supplier to get the HTML content
+                txtEditor.getStyleClass() // This is just a dummy dependency, replace with an appropriate property
+        ));
+
+        // Add a listener to the StringProperty
+        htmlContentProperty.addListener((observable, oldValue, newValue) -> {
+            calculateSearchResult();
+        });
+
+//        txtEditor.textProperty().addListener((ov, previous, current) -> calculateSearchResult());
+
 
         WebView webView = (WebView) txtEditor.lookup("WebView"); // Get internal WebView of  HTMLEditor
 
@@ -160,12 +201,29 @@ public class TextEditorFormController {
             }
         });
 
-
         mnuSelectAll.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 if (webEngine != null) {
                     webEngine.executeScript("document.body.focus(); document.execCommand('selectAll', false, null);"); // Execute a JavaScript script to select all texts
+                }
+            }
+        });
+
+        mnuFindAndReplace.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+                if (!pneContainer.getChildren().contains(pneFindAndReplace)) {
+                    pneContainer.getChildren().add(pneFindAndReplace); // add the TabPane from the layout
+
+                    if (pneFindAndReplace.getTabs().isEmpty()) {
+                        pneFindAndReplace.getTabs().add(pneTab);  // Re-add the tab to the TabPane if it's not already present
+                        pneFindAndReplace.getSelectionModel().select(pneTab); // Bring the tab to the front to make it active
+                    }
+
+                    // Adjust the top anchor of the htmlEditor to account for the FindAndReplace pane
+                    AnchorPane.setTopAnchor(txtEditor, AnchorPane.getTopAnchor(pneFindAndReplace) + pneFindAndReplace.getPrefHeight());
                 }
             }
         });
@@ -319,6 +377,8 @@ public class TextEditorFormController {
         }
     }
 
+    /* drag and drop text file on editor*/
+
     public void rootOnDragOver(DragEvent dragEvent) {
         Dragboard dragboard = dragEvent.getDragboard();
         if (dragboard.hasFiles()) {
@@ -347,5 +407,107 @@ public class TextEditorFormController {
         dragEvent.consume();
     }
 
+    /* find and replace */
 
+    private void calculateSearchResult() {
+        String query = txtFind.getText();
+        searchResultList.clear();
+        pos = 0;
+        deselectHtmlEditor();
+
+        Pattern pattern;
+        try {
+            pattern = Pattern.compile(query);
+        } catch (RuntimeException e) {
+            return;
+        }
+        Matcher matcher = pattern.matcher(getPlainTextFromHtmlEditor());
+
+        while (matcher.find()) {
+            int start = matcher.start();
+            int end = matcher.end();
+            SearchResult result = new SearchResult(start, end);
+            searchResultList.add(result);
+        }
+
+        lblResults.setText(String.format("%d Results", searchResultList.size()));
+
+        select();
+    }
+
+    private void deselectHtmlEditor() {
+        if (webEngine != null) {
+            webEngine.executeScript("window.getSelection().removeAllRanges();");  // Execute JavaScript to deselect the text
+        }
+    }
+
+    private String getPlainTextFromHtmlEditor() {
+        String htmlContent = txtEditor.getHtmlText(); // Get the HTML content
+        return htmlContent.replaceAll("<[^>]*>", "");
+    }
+
+
+    private void select() {
+        if (searchResultList.isEmpty()) return;
+        SearchResult searchResult = searchResultList.get(pos);
+        selectRangeInHtmlEditor(searchResult.getStart(), searchResult.getEnd());
+        lblResults.setText(String.format("%d/%d Results", (pos + 1), searchResultList.size()));
+    }
+
+
+    private void selectRangeInHtmlEditor(int start, int end) {
+
+        if (webEngine != null) {
+            // Inject JavaScript to select the text range
+            String script = String.format(
+                    "var range = document.createRange();" +
+                            "var selection = window.getSelection();" +
+                            "var node = document.body;" + // Assuming the content is within the body tag
+                            "range.setStart(node.firstChild, %d);" +  // Start position (character index)
+                            "range.setEnd(node.firstChild, %d);" +    // End position (character index)
+                            "selection.removeAllRanges();" +          // Clear any previous selection
+                            "selection.addRange(range);", start, end);
+
+
+            webEngine.executeScript(script); // Execute the JavaScript to select the text
+        }
+    }
+
+
+    public void btnDownOnAction(ActionEvent actionEvent) {
+        pos++;
+        if (pos == searchResultList.size()) {
+            pos = -1;
+            return;
+        }
+        select();
+    }
+
+    public void btnUpOnAction(ActionEvent actionEvent) {
+        pos--;
+        if (pos < 0) {
+            pos = searchResultList.size();
+            return;
+        }
+        select();
+    }
+
+    public void chkMatchCaseOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReplaceAllOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReplaceOnAction(ActionEvent actionEvent) {
+    }
+
+
+    public void pneTabOnClosed(Event event) {
+        removeTabPane();
+    }
+
+    private void removeTabPane(){
+        pneContainer.getChildren().remove(pneFindAndReplace); // Remove the TabPane from the layout
+        AnchorPane.setTopAnchor(txtEditor, AnchorPane.getTopAnchor(pneFindAndReplace)); // Move HTMLEditor up by setting the same top anchor as TabPane had
+    }
 }
